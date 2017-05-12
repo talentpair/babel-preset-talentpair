4aff800f3a421fd9f53a327e234f15d40af29eea
Ben Brandt <benjamin.j.brandt@gmail.com>
2017-05-12 12:34:51 -0700

    Redo env logic

--- index.js
+++ index.js
@@ -1,56 +1,62 @@
-module.exports = {
-  presets: [
-    [
-      require("babel-preset-env"),
-      {
-        targets: {
-          browsers: [">1%", "last 2 versions", "Firefox ESR"],
-          uglify: false
-        },
-        useBuiltIns: true,
-        modules: false
-      }
-    ],
-    require("babel-preset-react")
-  ],
-  plugins: [
-    require("babel-plugin-transform-class-properties"),
-    [require("babel-plugin-transform-object-rest-spread"), { useBuiltIns: true }],
-    [require("babel-plugin-transform-react-jsx"), { useBuiltIns: true }],
-    [
-      require("babel-plugin-transform-runtime"),
-      {
-        helpers: true,
-        polyfill: false,
-        regenerator: false,
-        moduleName: "babel-runtime"
-      }
-    ],
-    require("babel-plugin-syntax-dynamic-import")
-  ],
-  env: {
-    development: {
-      plugins: [
-        require("babel-plugin-transform-react-jsx-source"),
-        require("babel-plugin-transform-react-jsx-self")
-      ]
-    },
-    production: {
-      plugins: [
-        require("babel-plugin-transform-react-inline-elements"),
-        require("babel-plugin-transform-react-remove-prop-types")
-      ]
-    },
-    test: {
-      presets: [
-        [require("babel-preset-env"), { targets: { node: "current" } }],
-        require("babel-preset-react")
-      ],
-      plugins: [
-        require("babel-plugin-transform-react-jsx-source"),
-        require("babel-plugin-transform-react-jsx-self"),
-        require("babel-plugin-dynamic-import-node")
-      ]
+const env = process.env.NODE_ENV;
+
+const plugins = [
+  require.resolve("babel-plugin-transform-class-properties"),
+  [require.resolve("babel-plugin-transform-object-rest-spread"), { useBuiltIns: true }],
+  [require.resolve("babel-plugin-transform-react-jsx"), { useBuiltIns: true }],
+  [
+    require.resolve("babel-plugin-transform-runtime"),
+    {
+      helpers: true,
+      polyfill: false,
+      regenerator: false,
+      moduleName: "babel-runtime"
     }
-  }
-};
+  ],
+  require.resolve("babel-plugin-syntax-dynamic-import")
+];
+
+const presets = [
+  [
+    require.resolve("babel-preset-env"),
+    {
+      targets: {
+        browsers: [">1%", "last 2 versions", "Firefox ESR"],
+        uglify: false
+      },
+      useBuiltIns: true,
+      modules: false
+    }
+  ],
+  require.resolve("babel-preset-react")
+];
+
+if (env === "test") {
+  module.exports = {
+    presets: [
+      [require("babel-preset-env").default, { targets: { node: "current" } }],
+      require.resolve("babel-preset-react")
+    ],
+    plugins: plugins.concat([
+      require.resolve("babel-plugin-transform-react-jsx-source"),
+      require.resolve("babel-plugin-transform-react-jsx-self"),
+      require.resolve("babel-plugin-dynamic-import-node")
+    ])
+  };
+} else if (env === "production") {
+  module.exports = {
+    presets,
+    plugins: plugins.concat([
+      require.resolve("babel-plugin-transform-react-inline-elements"),
+      require.resolve("babel-plugin-transform-react-remove-prop-types")
+    ])
+  };
+} else {
+  module.exports = {
+    presets,
+    plugins: plugins.concat([
+      require.resolve("babel-plugin-transform-react-jsx-source"),
+      require.resolve("babel-plugin-transform-react-jsx-self")
+    ])
+  };
+}
